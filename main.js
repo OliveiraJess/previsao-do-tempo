eb6b00661551a508e999d2a7f702d8e18daf4a45
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-09-21 11:14:53 -0300

    pegando evento pelo click e pela tecla enter

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const city = document.querySelector('.city')
 const date = document.querySelector('.date');
 const mainImage = document.querySelector('.main_image');
-const containerTemperature = document. querySelector('.main_temperature');
+const containerTemperature = document.querySelector('.main_temperature');
 const temperature = document.querySelector('.temperature');
-const temperatureUnit = document.querySelector('.temperature span')
-const weather = document.querySelector('.weather');
+const temperatureUnit = document.querySelector('.main_temperature span')
+const weatherTemperature = document.querySelector('.weather');
 const maxMin = document.querySelector('.max-min');
-const searchInput = document.querySelector('search_input');
-const searchButton = document.querySelector('.ssearch_button');
+const searchInput = document.querySelector('.search_input');
+const searchButton = document.querySelector('.search_button');
 
 const api = {
     key: "ac4c350b9adb9fa9eb9271bc4b823c22",
@@ -15,3 +15,25 @@ const api = {
     lang: "pt_br",
     units: "metric"
 }
+
+
+
+searchInput.addEventListener('keypress', enter);
+
+function enter(event) {
+    if (event.code === "Enter") {
+        searchResults(searchInput.value)
+    }
+    console.log("enter")
+}
+
+searchButton.addEventListener('click', () => {
+    searchResults(searchInput.value)
+    console.log("clicado")
+});
+
+function searchResults(city) {
+    console.log("chamando funcao")
+
+}
+console.log(searchInput.value)
