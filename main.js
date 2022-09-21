9631eb51beda764b2041db05b670bfab11138b7b
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-09-21 13:59:57 -0300

    corrigindo requisicao api, mostrando resultados da api na tela e exibindo data de hoje

--- main.js
+++ main.js
@@ -29,21 +29,52 @@ function enter(event) {
 };
 
 function searchResults(city) {
-    fetch(`${api.base}weather?q=${city},&lang=${api.lang}&units=${api.units}&APPID=${api.key}`)
+    fetch(`${api.base}weather?q=${city}&lang=${api.lang}&units=${api.units}&APPID=${api.key}`)
         .then(response => {
             if (response.status == 200) {
-                displayResults(response)
-
+               return response.json();
             }
             else if (response.status != 200) {
-                throw new Error(`Algo deu errado: status ${response.status}`)
+                throw new Error(`Algo deu errado: status ${response.status}`);
             }
         })
         .catch(error => {
-            alert(error.message)
+            alert(error.message);
+        })
+        .then(response => {
+            displayResults(response);
         })
 };
 
 function displayResults(weather) {
     console.log(weather)
+
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    date.innerText = dateBuilder(now);
+
+    let iconName = weather.weather[0].icon;
+    mainImage.innerHTML = `<img src="./icons/${iconName}.png">`;
+
+    let temp = `${Math.round(weather.main.temp)}`
+    temperature.innerHTML = temp;
+    temperatureUnit.innerHTML = `°c`;
+
+    let weather_tempo = weather.weather[0].description;
+    weatherTemperature.innerText = capitalizeFirstLetter(weather_tempo);
+
+    maxMin.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+};
+
+function dateBuilder(d) {
+    let days = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+    let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julio", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}, ${date} ${month} ${year}`;
 }
